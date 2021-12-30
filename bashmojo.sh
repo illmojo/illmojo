7d4651d6a4724bffe4ea00351ebaa4b73c0b8ec1
illmojo <96870496+illmojo@users.noreply.github.com>
2021-12-30 13:36:53 +0200

    Create bashmojo.sh

---
+++ bashmojo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Way to use bash. mojotoitu:
+cmdsBashrc[0]="alias l='ls -lah'"
+cmdsBashrc[1]="alias 'cd..'='cd ..'"
+
+
+for cmd in "${cmdsBashrc[@]}"
+do
+  cutted=$(echo "$cmd" | cut -d "=" -f1)"="
+  if grep -q "$cutted" ~/.bashrc
+  then
+    echo "${cutted} #found, so not inserted to .bashrc"
+  else
+    echo "${cutted} #not found, so IS inserted to .bashrc"
+    $(echo "${cmd}" >> ~/.bashrc)
+  fi
+done
+
+# light came & light went
+echo "lets turn off the lights"
+echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null
+echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null
+
+if grep -q "\/sys\/class\/leds\/led0\/brightness" /etc/rc.local
+  then
+    echo "very dark already exists"
+  else
+    sudo sed -i -e '$i \sudo echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null\n' /etc/rc.local
+fi
+
+echo "leds off"

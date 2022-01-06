2a9b67c1f15a5150668be9bc9d78fc1bdd40ec87
illmojo <96870496+illmojo@users.noreply.github.com>
2022-01-06 09:36:45 +0200

    Update bashmojo.sh

--- bashmojo.sh
+++ bashmojo.sh
@@ -3,21 +3,22 @@
 # Way to use bash. mojotoitu:
 cmdsBashrc[0]="alias l='ls -lah'"
 cmdsBashrc[1]="alias 'cd..'='cd ..'"
-
+cmdsBashrc[2]="alias p=\"cal && echo '' && date '+%A %x' && date '+%X'\""
 
 for cmd in "${cmdsBashrc[@]}"
 do
   cutted=$(echo "$cmd" | cut -d "=" -f1)"="
-  if grep -q "$cutted" ~/.bashrc
+  if [[ $(grep "$cutted" ~/.bashrc ~/.bash_aliases | sed 's/^ *//g' | grep -v '^#') ]]
   then
-    echo "${cutted} #found, so not inserted to .bashrc"
+    echo -e "$cutted \t\t #found, so not inserted to .bash_aliases"
   else
-    echo "${cutted} #not found, so IS inserted to .bashrc"
-    $(echo "${cmd}" >> ~/.bashrc)
+    echo -e "$cutted \t\t #not found, so IS inserted to .bash_aliases"
+    $(sudo echo "$cmd" >> ~/.bash_aliases)
   fi
 done
 
 # light came & light went
+echo ""
 echo "lets turn off the lights"
 echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null
 echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null
@@ -26,14 +27,13 @@ if grep -q "\/sys\/class\/leds\/led0\/brightness" /etc/rc.local
   then
     echo "very dark already exists 0"
   else
-    sudo sed -i -e '$i \sudo echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null\n' /etc/rc.local
+    sudo sed -i '/^exit 0/i sudo echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null\n' /etc/rc.local
 fi
 
 if grep -q "\/sys\/class\/leds\/led1\/brightness" /etc/rc.local
   then
     echo "very dark already exists 1"
   else
-    sudo sed -i -e '$i \sudo echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null\n' /etc/rc.local
+    sudo sed -i '/^exit 0/i sudo echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null\n' /etc/rc.local
 fi
 echo "leds off"
-
